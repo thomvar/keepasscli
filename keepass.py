753825a6e7cca8cbdf60a01f6fddb5db4cf3d3ab
thomvar <thomvar01@protonmail.com>
2022-09-26 18:24:17 +0200

    Update documentation of `load_database`
    Remove previsous comment about brute-force and re-tries.

--- keepass.py
+++ keepass.py
@@ -37,8 +37,7 @@ def get_arguments() -> Namespace:
 
 
 def load_databases(file_name: str, new: bool=False) -> PyKeePass:
-    """Create or return the existing database. `try_again` opens up for brute-force attacks,
-    will use the 3-second rule between retries."""
+    """Create or return the existing database."""
     try_again = 0
     while try_again < 3:
         password = getpass("Database password: ")
